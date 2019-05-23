8cada2cf33e3daf38dd05418853aeae2cd0cce7a
Touhid Rahman <touhidrahman1987@gmail.com>
2019-05-24 01:35:29 +0200

    Add news service

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface News {
+  url: string;
+  headline?: string;
+  source?: string;
+  country?: string;
+  createdAt: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 
-  constructor() { }
+  private newsColletion: AngularFirestoreCollection<News>;
+
+  private newsList: Observable<News[]>;
+
+  constructor(private afs: AngularFirestore) {
+    this.newsColletion = this.afs.collection<News>('news');
+
+    this.newsList = this.newsColletion.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+      })
+    );
+  }
+
+  getNewsList(): Observable<Array<News>> {
+    return this.newsList;
+  }
+
+  getNews(id: string): Observable<News> {
+    return this.newsColletion.doc<News>(id).valueChanges();
+  }
+
+  updateNews(id: string, news: News) {
+    return this.newsColletion.doc(id).update(news);
+  }
+
+  addNews(news: News) {
+    return this.newsColletion.add(news);
+  }
+
+  removeNews(id: string) {
+    return this.newsColletion.doc(id).delete();
+  }
 }
