c4887323fa44db3aa1a6d54d5454ff9c52f16695
Touhid Rahman <touhidrahman1987@gmail.com>
2019-05-31 18:50:15 +0200

    Integrate FCM initially

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,25 @@
 
 export const environment = {
   production: false,
+  // test firebase config
+  // firebaseConfig: {
+  //   apiKey: "AIzaSyAi7nEjTzgO7tHkkk4ZAQF1HEmMoICe6CQ",
+  //   authDomain: "dev-test-thd.firebaseapp.com",
+  //   databaseURL: "https://dev-test-thd.firebaseio.com",
+  //   projectId: "dev-test-thd",
+  //   storageBucket: "dev-test-thd.appspot.com",
+  //   messagingSenderId: "476194911587",
+  //   appId: "1:476194911587:web:2353d4f092e5c8c1"
+  // },
+  // prod firebase config
   firebaseConfig: {
-    apiKey: "AIzaSyAi7nEjTzgO7tHkkk4ZAQF1HEmMoICe6CQ",
-    authDomain: "dev-test-thd.firebaseapp.com",
-    databaseURL: "https://dev-test-thd.firebaseio.com",
-    projectId: "dev-test-thd",
-    storageBucket: "dev-test-thd.appspot.com",
-    messagingSenderId: "476194911587",
-    appId: "1:476194911587:web:2353d4f092e5c8c1"
+    apiKey: "AIzaSyCBkLRDzYSwD05ci_sSttQ7AbwAaiWoHfc",
+    authDomain: "wwtabapp.firebaseapp.com",
+    databaseURL: "https://wwtabapp.firebaseio.com",
+    projectId: "wwtabapp",
+    storageBucket: "wwtabapp.appspot.com",
+    messagingSenderId: "862151947581",
+    appId: "1:862151947581:web:8c2d8d1ec38830ef"
   },
   // Google test ad IDs
   admobIds: {
